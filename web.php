71ea55c52b1fa91baa0d7859523431c416f0bfca
Ronny Wang <ronnywang@gmail.com>
2023-05-28 16:19:39 +0800

    加上 /law/xxx 頁面支援，並增加可以傳送 Param

--- web.php
+++ web.php
@@ -1,7 +1,25 @@
 <?php
 
+class Param
+{
+    protected static $values = null;
+    public static function set($k, $v)
+    {
+        self::$values[$k] = $v;
+    }
+
+    public static function get($k)
+    {
+        return self::$values[$k];
+    }
+}
+
 $uri = explode('?', $_SERVER['REQUEST_URI'])[0];
 if ($uri == '/' or $uri == '') {
     include(__DIR__ . '/page/main.php');
+    exit;
+} else if (preg_match('#^/law/(.*)$#', $uri, $matches)) {
+    Param::set('law_id', $matches[1]);
+    include(__DIR__ . '/page/law.php');
     exit;
 }
