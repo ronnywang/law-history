20a934af07acb386724318bcf4c53f95744b8b63
Ronny Wang <ronnywang@gmail.com>
2023-06-03 15:38:26 +0800

    增加介紹和程式碼連結

--- page/header.php
+++ page/header.php
@@ -13,7 +13,11 @@
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <a class="navbar-brand" href="/">法律查詢器</a>
-    </div>
+  </div>
+  <ul class="nav navbar-nav">
+      <li><a href="https://github.com/ronnywang/law-history" target="_blank">程式碼</a></li>
+      <li><a href="https://g0v.hackmd.io/Ds45AeOJQReU569q4pPIbQ" target="_blank">介紹共筆</a></li>
+  </ul>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
