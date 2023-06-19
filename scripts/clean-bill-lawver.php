fa74daefad985962c460e46f7f8f6febfa1be988
Ronny Wang <ronnywang@gmail.com>
2023-06-19 22:26:30 +0800

    清空所有議案條文

--- scripts/clean-bill-lawver.php
+++ scripts/clean-bill-lawver.php
@@ -7,3 +7,12 @@ foreach ($ret->lawver as $d) {
     $obj = API::query('law', '/lawver/' . urlencode($d->_id), 'DELETE', '');
     print_r($obj);
 }
+
+$ret = API::query('law', '/lawline/_search', 'POST', json_encode([
+    'query' => ['prefix' => ['法律版本代碼' => 'bill-']],
+]));
+foreach ($ret->hits->hits as $hit) {
+    $obj = API::query('law', '/lawline/' . urlencode($hit->_id), 'DELETE', '');
+    print_r($obj);
+}
+$ret = API::query('law', '/_flush', 'POST', '');
